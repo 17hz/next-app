604021b2b8a64d4ce2736107d98ef2589db98aa6
17hz <0x149527@gmail.com>
2024-09-13 17:31:03 +0800

    Add dynamic route for hello API and set nodejs runtime

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,4 +1,6 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+// 设置 nodejs 运行时
+export const runtime = 'nodejs';
 
 export function GET() {
   return new Response(`Hello from ${process.env.VERCEL_REGION}`);
